a7d83f732a1db306a124034a84200d1ddecd5aee
James <jameslaurencebelen@gmail.com>
2025-03-31 19:03:47 +0800

    added finduserpayment for payment repository

--- src/main/java/health/medikeep/appointment/users/PaymentRepository.java
+++ src/main/java/health/medikeep/appointment/users/PaymentRepository.java
@@ -1,5 +1,7 @@
 package health.medikeep.appointment.users;
 
+import java.util.Optional;
+
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.Assert;
@@ -7,9 +9,11 @@ import org.springframework.util.Assert;
 @Repository
 public class PaymentRepository {
     private final JdbcClient jdbcClient;
+    private final UserRepository userRepository;
     
-    public PaymentRepository(JdbcClient jdbcClient){
+    public PaymentRepository(JdbcClient jdbcClient, UserRepository userRepository) {
         this.jdbcClient = jdbcClient;
+        this.userRepository = userRepository;
     }
 
     public void create(PaymentInfo paymentInfo){
@@ -19,4 +23,16 @@ public class PaymentRepository {
 
         Assert.state(updated == 1, "Payment Failed");
     }
+    // So, dito find by email sya pero nangyayari jan is hahanapin ung id by email then gagamittin ung nakuhang id para makuha ung payments
+    // i know na complicated sya pero naka foreign key kasi dun sa table ng payments is ung user id HAHAHAHAHAH
+    public Optional<PaymentInfo> findUserPayments(String email){
+        return jdbcClient.sql("select * from payments where id = :id")
+            .param("id", userRepository.getUid(email))
+            .query(PaymentInfo.class)  
+            .optional();
+    }
+
+    //to do: find payments by date
+
+    
 }
