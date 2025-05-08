b90044c2f267cc771023333380182ff10b935718
James <jameslaurencebelen@gmail.com>
2025-05-08 16:55:20 +0800

    created an admin login security for admin page

--- src/main/java/health/medikeep/appointment/users/AdminController.java
+++ src/main/java/health/medikeep/appointment/users/AdminController.java
@@ -5,7 +5,11 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
 
 import org.springframework.ui.Model;
 
@@ -28,8 +32,41 @@ public class AdminController {
         this.affiliateRepository = affiliateRepository;
     }
 
+    @PostMapping("/login")
+    public String login(@RequestParam String password, HttpSession session){
+        System.out.println(password);
+        if(password.equals("root")){
+            session.setAttribute("role", "admin");
+            System.out.println("Session role: " + session.getAttribute("role"));
+
+            return "/admin/admin-home";
+            
+        } else {
+            System.out.println("Session role: " + session.getAttribute("role"));
+
+            return "/admin/";
+        }
+        
+    }
+    
+    @GetMapping("/")
+    public String login(HttpSession session){
+        System.out.println("Session role: " + session.getAttribute("role"));
+
+        if(session.getAttribute("role") != null && session.getAttribute("role").equals("admin")){
+            return "/admin/admin-home";
+        } else {
+            return "/admin/admin-login"; 
+        }
+        
+
+    }
+
     @GetMapping("/all") // Changed the HTML for user and doctor
-    public String showAll(Model model){
+    public String showAll(Model model, HttpSession session){
+        if(session.getAttribute("role") == null || !session.getAttribute("role").equals("admin")){
+            return "/admin/admin-login";
+        } 
         List<UserInfo> users = userRepository.showUsers();
         List<DoctorInfo> doctors = doctorRepository.showDoctors();
         
@@ -40,28 +77,40 @@ public class AdminController {
     }
 
     @GetMapping("/doctors")
-    public String showDoctoctors(Model model){
+    public String showDoctoctors(Model model, HttpSession session){
+        if(session.getAttribute("role") == null || !session.getAttribute("role").equals("admin")){
+            return "/admin/admin-login";
+        }
         List<DoctorInfo> doctors = doctorRepository.showDoctors();
         model.addAttribute("doctors", doctors);
         return "/admin/admin-doctor";
     }
 
     @GetMapping("/doctor/{doctor_id}")
-    public String showDoctor(@PathVariable Integer doctor_id, Model model){
+    public String showDoctor(@PathVariable Integer doctor_id, Model model, HttpSession session){
+        if(session.getAttribute("role") == null || !session.getAttribute("role").equals("admin")){
+            return "/admin/admin-login";
+        }
         DoctorInfo doctor = doctorRepository.findById(doctor_id).orElseThrow(() -> new RuntimeException("Doctor not found"));
         model.addAttribute("doctor", doctor);
         return "/admin/admin-doctor-info";
     }
 
     @GetMapping("/user/{user_id}")
-    public String showUser(@PathVariable Integer user_id, Model model){
+    public String showUser(@PathVariable Integer user_id, Model model, HttpSession session){
+        if(session.getAttribute("role") == null || !session.getAttribute("role").equals("admin")){
+            return "/admin/admin-login";
+        }
         UserInfo user = userRepository.findById(user_id).orElseThrow(() -> new RuntimeException("User not found"));
         model.addAttribute("user", user);
         return "/admin/admin-user-info";
     }
 
     @GetMapping("/users")
-    public String showUsers(Model model){
+    public String showUsers(Model model, HttpSession session){
+        if(session.getAttribute("role") == null || !session.getAttribute("role").equals("admin")){
+            return "/admin/admin-login";
+        }
         List<UserInfo> users = userRepository.showUsers();
         model.addAttribute("users", users);
         return "/admin/admin-user";
@@ -70,26 +119,38 @@ public class AdminController {
     
 
     @GetMapping("/affiliates")
-    public String showAffiliates(Model model){
+    public String showAffiliates(Model model, HttpSession session){
+        if(session.getAttribute("role") == null || !session.getAttribute("role").equals("admin")){
+            return "/admin/admin-login";
+        }
         List<AffiliateInfo> affiliates = affiliateRepository.showAffiliates();
         model.addAttribute("affiliates", affiliates);
         return "/admin/admin-affiliates";
     }
 
     @GetMapping("/affiliates/create")
-    public String createAffiliates(){
+    public String createAffiliates(HttpSession session){
+        if(session.getAttribute("role") == null || !session.getAttribute("role").equals("admin")){
+            return "/admin/admin-login";
+        }
         return "/admin/admin-affiliate-create";
     }
 
     @GetMapping("/appointments")
-    public String showAppointments(Model model){
+    public String showAppointments(Model model, HttpSession session){
+        if(session.getAttribute("role") == null || !session.getAttribute("role").equals("admin")){
+            return "/admin/admin-login";
+        }
         List<AppointmentInfo> appointments = appointmentRepository.showAppointments();
         model.addAttribute("appointments", appointments);
         return "/admin/admin-all-appointments";
     }
 
     @GetMapping("/billings")
-    public String showBillings(Model model){
+    public String showBillings(Model model, HttpSession session){
+        if(session.getAttribute("role") == null || !session.getAttribute("role").equals("admin")){
+            return "/admin/admin-login";
+        }
         List<BillingInfo> billings = billingRepository.showBillings();
         model.addAttribute("billings", billings);
         return "/admin/admin-billings";
